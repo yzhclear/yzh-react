66160cda494eb86ce6ea4f24aa8b312926f0fc17
yzhclear <188225829@qq.com>
2024-03-23 17:36:06 +0800

    fix: 同步任务队列执行完成后置空

--- packages/react-reconciler/src/syncTaskQueue.ts
+++ packages/react-reconciler/src/syncTaskQueue.ts
@@ -1,26 +1,27 @@
-let syncTaskQueue: ((...arg: any) => void)[] | null = null
-let isFlushingSyncQueue: boolean = false
+let syncTaskQueue: ((...arg: any) => void)[] | null = null;
+let isFlushingSyncQueue: boolean = false;
 
 export function scheduleSyncCallback(callback: (...arg: any) => void) {
-  if (syncTaskQueue === null) {
-    syncTaskQueue = [callback]
-  } else {
-    syncTaskQueue.push(callback)
-  }
+	if (syncTaskQueue === null) {
+		syncTaskQueue = [callback];
+	} else {
+		syncTaskQueue.push(callback);
+	}
 }
 
 export function flushSyncCallbacks() {
-  if (!isFlushingSyncQueue && syncTaskQueue) {
-    isFlushingSyncQueue = true
+	if (!isFlushingSyncQueue && syncTaskQueue) {
+		isFlushingSyncQueue = true;
 
-    try {
-      syncTaskQueue.forEach(callback => callback())
-    } catch (error) {
-      if (__DEV__) {
-        console.warn('flushSyncCallbacks报错', error)
-      }
-    }finally {
-      isFlushingSyncQueue = false
-    }
-  }
+		try {
+			syncTaskQueue.forEach((callback) => callback());
+		} catch (error) {
+			if (__DEV__) {
+				console.warn('flushSyncCallbacks报错', error);
+			}
+		} finally {
+			isFlushingSyncQueue = false;
+      syncTaskQueue = null
+		}
+	}
 }
